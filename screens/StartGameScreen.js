26efde56228971907a929c754651ccaa4435f8f1
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-21 18:50:03 -0500

    added buttons and input to game screen component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game Screen!</Text>
+            <Text>Start a New Game</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.text}>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => {}}></Button>
+                    <Button title="Confirm" onPress={() => {}}></Button>
+                </View>
+
+            </View>
         </View>
     )
 };
@@ -13,8 +22,22 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        alignItems: 'center',
-    }
+        alignItems: 'center'
+    },
+    text: {
+        alignItems: 'center'
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
 });
 
 export default StartGameScreen;
