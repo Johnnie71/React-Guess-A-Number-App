748dff2b95a6b860f957b32c99c3564a7ad61fd1
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-21 19:05:31 -0500

    added styling for input and buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,13 +30,21 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 10,
+        padding: 20,
+        borderRadius: 10,
     },
     buttonContainer: {
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-between',
-        paddingHorizontal: 15
+        paddingHorizontal: 15,
     },
 });
 
