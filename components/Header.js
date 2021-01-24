623d3ab30884f756ae92852198492e9b1878cfd7
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-23 19:24:22 -0500

    added styles to TitleText component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Colors from '../constants/colors'
+import Colors from '../constants/colors';
+import TitleText from '../components/TitleText';
 
 const Header = props => {
     return (
         <View style={styles.header}>
-            <Text style={styles.headerTitle}>{props.title}</Text>
+            <TitleText>{props.title}</TitleText>
         </View>
     );
 };
@@ -19,11 +20,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    headerTitle: {
-        color: 'black', 
-        fontSize: 18,
-        fontFamily: 'open-sans-bold',
-    }
 });
 
 export default Header;
