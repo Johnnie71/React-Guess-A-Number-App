b73c93e94c4902ef07dcb33a6ba268d7fefabde3
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-25 18:57:16 -0500

    used dimensions and an if ternary

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { View,
+     Text,
+     StyleSheet,
+     Alert,
+     ScrollView,
+     FlatList,
+     Dimensions } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -101,7 +107,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 450,
         maxWidth: '90%',
     },
