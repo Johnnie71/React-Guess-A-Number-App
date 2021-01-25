a8ed6c7465fd2b89be7b8f65fc724bdec4d16709
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-25 18:44:11 -0500

    used percentages to help with multiple screen sizes

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         width: '80%',
-        // maxWidth: '80%',
+        maxWidth: '95%',
         minWidth: 300,
         alignItems: 'center'
     },
