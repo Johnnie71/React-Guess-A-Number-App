e8b604e46174d395a40686504fc688569ef0a1bc
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-25 19:24:53 -0500

    added scroll view to game over sreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,7 +3,8 @@ import { View, StyleSheet,
      Button,
      Image,
      Text,
-     Dimensions
+     Dimensions,
+     ScrollView
  } from 'react-native';
 
 import BodyText from '../components/BodyText';
@@ -14,27 +15,29 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <TitleText>The Game is Over!</TitleText>
-            <View style={styles.imageContainer}>
-                <Image 
-                fadeDuration={1000}
-                // source={require('../assets/images/success.png')}
-                source={{uri: 'https://artofcreativephotography.com/wp-content/uploads/2019/03/David-Koester-Start-In-Die-Landschaftsfotografie-I.jpg'}}
-                style={styles.image}
-                resizeMode="cover"
-                />
+        <ScrollView>
+            <View style={styles.screen}>
+                <TitleText>The Game is Over!</TitleText>
+                <View style={styles.imageContainer}>
+                    <Image 
+                    fadeDuration={1000}
+                    // source={require('../assets/images/success.png')}
+                    source={{uri: 'https://artofcreativephotography.com/wp-content/uploads/2019/03/David-Koester-Start-In-Die-Landschaftsfotografie-I.jpg'}}
+                    style={styles.image}
+                    resizeMode="cover"
+                    />
+                </View>
+                <View style={styles.resultContainer} >
+                    <BodyText style={styles.resultText} >
+                        Your phone needed <Text style={styles.highlight} >{props.roundsNumber}</Text> rounds to guess the
+                        number <Text style={styles.highlight} >{props.userNumber}</Text>.
+                    </BodyText>
+                </View>
+                <MainButton onPress={props.onRestart}>
+                    NEW GAME
+                </MainButton>
             </View>
-            <View style={styles.resultContainer} >
-                <BodyText style={styles.resultText} >
-                    Your phone needed <Text style={styles.highlight} >{props.roundsNumber}</Text> rounds to guess the
-                    number <Text style={styles.highlight} >{props.userNumber}</Text>.
-                </BodyText>
-            </View>
-            <MainButton onPress={props.onRestart}>
-                NEW GAME
-            </MainButton>
-        </View>
+        </ScrollView>
 
     ) 
 };
