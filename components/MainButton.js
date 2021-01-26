15145a5e431d4c22cc36e734e046dbc3d62920bf
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-26 17:05:50 -0500

    added button container style for buttons

--- components/MainButton.js
+++ components/MainButton.js
@@ -16,14 +16,22 @@ const MainButton = props => {
     }
 
 
-    return <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonText} >{props.children}</Text>
-                </View>
-           </ButtonComponent>
+    return (
+     <View style={styles.buttonContainer}>
+        <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText} >{props.children}</Text>
+            </View>
+        </ButtonComponent>
+     </View>
+    );
 };
 
 const styles = StyleSheet.create({
+    buttonContainer:{
+        borderRadius: 25,
+        overflow: 'hidden'
+    },
     button: {
         backgroundColor: Colors.primary,
         paddingVertical: 12,
